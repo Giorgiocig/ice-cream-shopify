76cdc8a91895370fc99b8c5a181ad9fce1560203
Giorgiocig <giorgiocignitti@gmail.com>
2025-10-13 11:56:32 +0200

    Remove unused console.log

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import Image from "next/image";
 import { HeroSection } from "./components/layout";
-import { getAllProducts } from "./storefrontApi";
 
 export default async function Home() {
-  /* const response = await getAllProducts();
-  console.log(response); */
   return (
     <div>
       <HeroSection />
