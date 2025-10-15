c30e30599baf3bd872027701c3e1406300628a98
Giorgiocig <giorgiocignitti@gmail.com>
2025-10-15 11:25:56 +0200

    Implemetn query to update cart line and quantity

---
+++ app/storefrontApi/cartLinesUpdate.ts
@@ -0,0 +1,52 @@
+import { shopifyFetch } from "../utils/fetch-shopify";
+
+const gql = String.raw;
+
+export async function cartLinesUpdate(cartId: string, lines: any[]) {
+  return shopifyFetch({
+    query: gql`
+      mutation cartLinesUpdate($cartId: ID!, $lines: [CartLineUpdateInput!]!) {
+        cartLinesUpdate(cartId: $cartId, lines: $lines) {
+          cart {
+            id
+            lines(first: 20) {
+              edges {
+                node {
+                  id
+                  quantity
+                  merchandise {
+                    ... on ProductVariant {
+                      id
+                      title
+                      price {
+                        amount
+                        currencyCode
+                      }
+                      product {
+                        title
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            estimatedCost {
+              totalAmount {
+                amount
+                currencyCode
+              }
+            }
+          }
+          userErrors {
+            field
+            message
+          }
+        }
+      }
+    `,
+    variables: {
+      cartId,
+      lines,
+    },
+  });
+}
